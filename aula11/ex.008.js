5b9ce329275b083c96cda9656b6ad07c846ccecb
Hugo Antonio <hugoantonio18244@gmail.com>
2022-07-11 00:01:57 -0300

    Aula 11 e 12

---
+++ aula11/ex.008.js
@@ -0,0 +1,6 @@
+var vel = 60.5;
+console.log(`Sua velocidade é ${vel}`);
+if(vel > 60){
+    console.log('Voce ultrapassou a velocidade limite, MULTADO!!');
+}
+console.log(`Dirija sempre usando cinto de segurança`);
