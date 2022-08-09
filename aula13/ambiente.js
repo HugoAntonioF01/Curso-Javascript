8d1cfaaa9e890ba10a51a31e04ed392de3b4253b
Hugo Antonio <hugoantonio18244@gmail.com>
2022-08-08 23:47:52 -0300

    Pratica com repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,21 @@
+/*console.log("Ola")
+console.log("Ola")
+console.log("Ola")
+console.log("Ola")
+console.log("Ola")
+console.log("Ola")
+*/
+/*var c = 1;
+while(c <= 6){
+    console.log("Olá");
+    c++
+} */
+/*var c = 1;
+do{
+    console.log(`Olá ${c}`)
+    c++
+} while(c <= 6)
+*/
+for(var c = 1; c <= 6, c++){
+    console.log("olá")
+}
