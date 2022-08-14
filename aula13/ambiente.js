2d32a8b2c3d4f1dc9a88d6820c24f1b65ef9c3fb
Hugo Antonio <hugoantonio18244@gmail.com>
2022-08-14 01:18:25 -0300

    Estrutura de repeticao com FOR

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -10,12 +10,11 @@ while(c <= 6){
     console.log("Olá");
     c++
 } */
-/*var c = 1;
+var c = 1;
 do{
     console.log(`Olá ${c}`)
     c++
 } while(c <= 6)
-*/
-for(var c = 1; c <= 6, c++){
-    console.log("olá")
-}
+
+
+
---
+++ aula14/ambiente2.js
@@ -0,0 +1,5 @@
+console.log("Vai começar")
+for(c = 1; c <= 10; c = c + 1){
+    console.log("Cambalhora" + c)
+}
+console.log("Fim")
